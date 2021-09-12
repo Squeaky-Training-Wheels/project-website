01d894b44d1ae73d88bc0dd5a886217d4125fc4f
Elijah Parrish <elijah.parrish321@gmail.com>
2021-09-12 17:08:51 -0400

    Ran create-react-app and pasted the synopsis into the site. We're off the ground!

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        RIT Senior Project - Palmyra Racing Association
+      </header>
+      <h1></h1>
+      <h3>Project Synopsis</h3>
+      <p>
+        The Palmyra Racing Association Club Manager is a web and mobile application sponsored by the Palmyra Racing Association in order to facilitate the management of many facets of the club, including membership, member participation, and event tracking and signups.
+        This project replaces an existing application which is already fairly outdated by web standards and is in need of a visual touch up. One of the primary goals of the application is ease of use and accessibility in order to get non-tech-savvy club members to use the application.
+        Club members can use the application for updating their personal information, renewing their membership, and signing up to volunteer at events. Club board members can use the application to view and edit membership data for all active members as well as signup information for every event. 
+        Board members can also create new events and update existing ones, as well as assign club members to events manually. These features are currently provided via separate applications, and this application seeks to unify them into one easy to use, modernized user interface.
+      </p>
+    </div>
+  );
+}
+
+export default App;
