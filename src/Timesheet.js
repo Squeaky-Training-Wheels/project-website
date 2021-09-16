833685281699e5a99365f68c8c9ba77312bc6327
Elijah Parrish <elijah.parrish321@gmail.com>
2021-09-15 22:29:29 -0400

    fixed timesheet.. again

--- src/Timesheet.js
+++ src/Timesheet.js
@@ -7,10 +7,11 @@ class Timesheet extends React.Component {
 
     render() {
         const { sheetName, caption } = this.props;
+        console.log(sheetName);
         return (
             <div>
                 <h4>{sheetName}</h4>
-                <img src={`./timesheets/${sheetName}`}></img>
+                <img src={`./timesheets/${sheetName}.png`}></img>
                 <br />
                 <span style={{ textAlign: 'center' }}>{caption}</span>
             </div>
