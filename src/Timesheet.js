639ea82e06c660d946f9b99c59bff951042fca39
matedro <matedro1@gmail.com>
2021-09-17 13:41:24 -0400

    Resolved warnings to allow CD to pass

--- src/Timesheet.js
+++ src/Timesheet.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 class Timesheet extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         const { sheetName, caption } = this.props;
         console.log(sheetName);
         return (
             <div>
                 <h4>{sheetName}</h4>
-                <img src={`./timesheets/${sheetName}.png`}></img>
+                <img src={`./timesheets/${sheetName}.png`} alt={sheetName} />
                 <br />
                 <span style={{ textAlign: 'center' }}>{caption}</span>
             </div>
