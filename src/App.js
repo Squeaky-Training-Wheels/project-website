6ce794be6c1d7978eb207142d59a82a142903faa
Elijah Parrish <elijah.parrish321@gmail.com>
2021-09-23 21:12:41 -0400

    Image carousels working in accordion components, pdf displaying two pages at a time and centered

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import FourUp from './accordionComponents/FourUp';
 import timesheets from './accordionData/timesheets.json'
 import fourUps from './accordionData/fourups.json'
 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 import { Document, Page, pdfjs } from "react-pdf";
 
 import {
@@ -15,7 +18,9 @@ import {
     AccordionPanel,
     Box,
     Heading, Image,
-    Text
+    Text,
+    Wrap,
+    Center
 } from "@chakra-ui/react";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -24,7 +29,7 @@ function App() {
   return (
     <Box className="App" bg="brand.700" color="brand.800">
             <Heading bg="brand.100" color="brand.700">
-                <Text fontSize="6xl">RIT Senior Project - Palmyra Racing Association Club Manager</Text>
+                <Text fontSize="4xl">RIT Senior Project - Palmyra Racing Association Club Manager</Text>
             </Heading>
             <Image display="flex" h="700px" w={"100%"} src="https://wallpaperstock.net/motocross-racing_wallpapers_46236_852x480.jpg"/>
 
@@ -94,11 +99,13 @@ function App() {
                         </AccordionButton>
                     </h2>
                     <AccordionPanel pb={4} bg="brand.600">
-                        {
-                            timesheets.map((timesheet) => (
-                                <Timesheet h="700px" w={"100%"} key={timesheet.sheetName} sheetName={timesheet.sheetName} caption={timesheet.caption}/>
-                            ))
-                        }
+                        <Carousel>
+                            {
+                                timesheets.map((timesheet) => (
+                                    <Timesheet key={timesheet.sheetName} sheetName={timesheet.sheetName} caption={timesheet.caption}/>
+                                ))
+                            }
+                        </Carousel>
                     </AccordionPanel>
                 </AccordionItem>
                 <AccordionItem>
@@ -111,11 +118,13 @@ function App() {
                         </AccordionButton>
                     </h2>
                     <AccordionPanel pb={4} bg="brand.600">
-                        {
-                            fourUps.map((fourup) => (
-                                <FourUp h="700px" w={"100%"} key={fourup.fourUpName} fourUpName={fourup.fourUpName} caption={fourup.caption}/>
-                            ))
-                        }
+                        <Carousel>
+                            {
+                                fourUps.map((fourup) => (
+                                    <FourUp key={fourup.fourUpName} fourUpName={fourup.fourUpName} caption={fourup.caption}/>
+                                ))
+                            }
+                        </Carousel>
                     </AccordionPanel>
                 </AccordionItem>
                 <AccordionItem >
@@ -128,21 +137,20 @@ function App() {
                         </AccordionButton>
                     </h2>
                     <AccordionPanel pb={4} bg="brand.600">
-                    <Document
-                        file={process.env.PUBLIC_URL + '/ProjectPlan.pdf'}
-                    >
-                        <Page pageNumber={1}/>
-                        <br/>
-                        <Page pageNumber={2}/>
-                        <br/>
-                        <Page pageNumber={3}/>
-                        <br/>
-                        <Page pageNumber={4}/>
-                        <br/>
-                        <Page pageNumber={5}/>
-                        <br/>
-                        <Page pageNumber={6}/>
-                    </Document>
+                        <Center>
+                            <Document
+                                file={process.env.PUBLIC_URL + '/ProjectPlan.pdf'}
+                            >
+                                <Wrap justify="center">
+                                    <Page pageNumber={1}/>
+                                    <Page pageNumber={2}/>
+                                    <Page pageNumber={3}/>
+                                    <Page pageNumber={4}/>
+                                    <Page pageNumber={5}/>
+                                    <Page pageNumber={6}/>
+                                </Wrap>
+                            </Document>
+                        </Center>
                     </AccordionPanel>
                 </AccordionItem>
             </Accordion>
--- src/accordionComponents/FourUp.js
+++ src/accordionComponents/FourUp.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Image } from '@chakra-ui/image';
+import { Box, Center, Image, VStack } from "@chakra-ui/react";
 
 class FourUp extends React.Component {
     render() {
-        const { fourUpName, caption } = this.props       
+        const { fourUpName, caption } = this.props
         return (
-            <div>
-                <h4>{fourUpName}</h4>
-                <Image src={process.env.PUBLIC_URL + `/images/${fourUpName}.png`} alt={fourUpName} />
-                <br />
-                <span style={{ textAlign: 'center' }}>{caption}</span>
-            </div>
+            <Center p="5">
+                <VStack> 
+                    <Box width="100%">
+                        <Image height="400" src={process.env.PUBLIC_URL + `/images/${fourUpName}.png`} alt={fourUpName} />
+                    </Box>   
+                    <Box p="5"> 
+                        <span style={{ textAlign: 'center' }}>{caption}</span>
+                    </Box>  
+                </VStack> 
+            </Center>
         );
     }
 }
