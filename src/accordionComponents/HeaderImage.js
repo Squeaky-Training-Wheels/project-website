55d52b7e6265eabb350687b587ca34615009408e
cjs8487 <cjschaefer1999@gmail.com>
2021-10-03 13:21:32 -0400

    style: HeaderImage.js

--- src/accordionComponents/HeaderImage.js
+++ src/accordionComponents/HeaderImage.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Image, VStack, Box, Center } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
 
 class HeaderImage extends React.Component {
     render() {
         const { imageName } = this.props;
         return (
             <Center p="5">
-                <VStack> 
+                <VStack>
                     <Box width="100%">
-                        <Image height="600" src={process.env.PUBLIC_URL + `/images/${imageName}.png`} alt={imageName} />
-                    </Box>   
-                </VStack> 
+                        <Image height="600" src={`${process.env.PUBLIC_URL}/images/${imageName}.png`} alt={imageName} />
+                    </Box>
+                </VStack>
             </Center>
         );
     }
 }
+
+HeaderImage.propTypes = {
+    imageName: PropTypes.string.isRequired,
+};
 
 export default HeaderImage;
