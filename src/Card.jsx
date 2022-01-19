c981426652ff7472c0224c40a736aa24566d9559
Elijah Parrish <elijah.parrish321@gmail.com>
2022-01-18 23:55:37 -0500

    Styled icons in cards

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,58 +1,82 @@
-/* eslint-disable max-len */
 import React from 'react';
 import {
     Heading,
     Avatar,
-    Box,
-    Center,
     Flex,
     Text,
-    Stack,
+    // Stack,
+    IconButton,
 } from '@chakra-ui/react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import PropTypes from 'prop-types';
 
 export default function Card(props) {
     const { name, roles, picture } = props;
+
+    function displayCardIcons() {
+        if (roles[0] !== 'Faculty Coach' && roles[0] !== 'Sponsor') {
+            return (
+                <Flex marginTop="auto" marginBottom={2} justify="center">
+                    <IconButton
+                        size="sm"
+                        variant="ghost"
+                        p="0"
+                        fontSize="24px"
+                        color="brand.200"
+                        icon={<AiFillGithub />}
+                    />
+                    <IconButton
+                        size="sm"
+                        variant="ghost"
+                        p="0"
+                        fontSize="24px"
+                        color="brand.200"
+                        icon={<AiFillLinkedin />}
+                    />
+                </Flex>
+            );
+        }
+        return <div />; // else return nothing
+    }
+
     return (
-        <Center py={6}>
-            <Box
-                minW="200px"
-                maxW="200px"
-                w="full"
-                minH="250px"
-                maxH="250px"
-                h="full"
-                bg="brand.600"
-                rounded="md"
-                overflow="hidden"
+        <Flex
+            minW="230px"
+            maxW="230px"
+            w="full"
+            minH="255px"
+            maxH="255px"
+            h="full"
+            bg="brand.600"
+            rounded="md"
+            overflow="hidden"
+            direction="column"
+            alignItems="center"
+        >
+            <Avatar
+                m={3}
+                size="xl"
+                src={`${process.env.PUBLIC_URL}/images/${picture}`}
+                alt="Author"
+            />
+            <Heading
+                justify="center"
+                fontSize="2xl"
+                fontWeight="semibold"
+                fontFamily="body"
+                color="brand.200"
+                textColor="brand.300"
+                mb={1}
             >
-                <Box px={6}>
-                    <Flex justify="center" mt={3} pb={3}>
-                        <Avatar
-                            size="xl"
-                            src={`${process.env.PUBLIC_URL}/images/${picture}`}
-                            alt="Author"
-                        />
-                    </Flex>
-                    <Stack spacing={0} align="center" mb={5}>
-                        <Heading
-                            fontSize="2xl"
-                            fontWeight="semibold"
-                            fontFamily="body"
-                            color="brand.200"
-                            textColor="brand.300"
-                        >
-                            {name}
-                        </Heading>
-                        {
-                            roles.map((role) => (
-                                <Text key={`role-${role}`} color="brand.200">{role}</Text>
-                            ))
-                        }
-                    </Stack>
-                </Box>
-            </Box>
-        </Center>
+                {name}
+            </Heading>
+            {
+                roles.map((role) => (
+                    <Text lineHeight={1.2} fontSize="15px" key={`role-${role}`} color="brand.200">{role}</Text>
+                ))
+            }
+            { displayCardIcons() }
+        </Flex>
     );
 }
 
